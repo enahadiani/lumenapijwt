a5e58caaf95de9b08a63326219b54cc007b7a507
= <=>
2020-03-27 09:52:05 +0700

    tambah dev tagihan api

--- routes/web.php
+++ routes/web.php
@@ -52,6 +52,10 @@ $router->group(['prefix' => 'api'], function () use ($router) {
     $router->put('/siswa/{id}','DevSiswaController@update');
     $router->delete('/siswa/{id}','DevSiswaController@destroy');
 
+    //tgh
+    $router->get('/tgh','DevTagihanController@index');
+    $router->post('/tgh','DevTagihanController@store');
+
 
 });
  
